90db012a526552c9f7956a3191b11a58367868a0
osama <enlil96rashid@gmail.com>
2018-07-11 15:55:43 +0200

    added the demo data

---
+++ db.js
@@ -0,0 +1,42 @@
+export const data = {
+  1: {
+    catagory: "agency",
+    title: "title 1",
+    image: "https://www.w3schools.com/w3css/img_lights.jpg",
+    content:
+      "some content, posd spd soid sdoins disodv odifs dfoisddg soinsjdsifskdf sdfosidn",
+    date: "10/10/2010"
+  },
+  12: {
+    catagory: "bank",
+    title: "title 12",
+    image: "https://www.w3schools.com/w3css/img_lights.jpg",
+    content:
+      "some content, posd spd soid sdoins disodv odifs dfoisddg soinsjdsifskdf sdfosidn",
+    date: "10/10/2010"
+  },
+  123: {
+    catagory: "tax",
+    title: "title 123",
+    image: "https://www.w3schools.com/w3css/img_lights.jpg",
+    content:
+      "some content, posd spd soid sdoins disodv odifs dfoisddg soinsjdsifskdf sdfosidn",
+    date: "10/10/2010"
+  },
+  1234: {
+    catagory: "expenses",
+    title: "title 1234",
+    image: "https://www.w3schools.com/w3css/img_lights.jpg",
+    content:
+      "some content, posd spd soid sdoins disodv odifs dfoisddg soinsjdsifskdf sdfosidn",
+    date: "10/10/2010"
+  },
+  12345: {
+    catagory: "other",
+    title: "title 12345",
+    image: "https://www.w3schools.com/w3css/img_lights.jpg",
+    content:
+      "some content, posd spd soid sdoins disodv odifs dfoisddg soinsjdsifskdf sdfosidn",
+    date: "10/10/2010"
+  }
+};
