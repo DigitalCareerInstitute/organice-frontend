b4f3d07665f5d45f02e01062a0be49931e78e942
majd <majdissa968@gmail.com>
2018-07-20 13:09:15 +0200

    editing the log in form with designing

--- components/LogInForm.js
+++ components/LogInForm.js
@@ -1,66 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-	TouchableOpacity,
 	StyleSheet,
 	Text,
 	View,
-	TextInput
+	TextInput,
+	TouchableHighlight,
+	Alert
 } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
-const styles = StyleSheet.create({
-	regForm: {
-		alignSelf: 'stretch'
-	},
-	header: {
-		fontSize: 20,
-		color: '#fff',
-		paddingBottom: 10,
-		marginBottom: 40,
-		borderBottomColor: '#199187',
-		borderBottomWidth: 1
-	},
-	textInput: {
-		alignSelf: 'stretch',
-		height: 40,
-		marginBottom: 30,
-		color: '#fff',
-		borderBottomColor: '#f8f8f8',
-		borderBottomWidth: 1
-	},
-	button: {
-		alignSelf: 'stretch',
-		alignItems: 'center',
-		padding: 20,
-		backgroundColor: '#0fa1ff',
-		marginTop: 30
-	},
-	btnText: {
-		color: '#fff',
-		fontWeight: 'bold'
+export default class LogInForm extends Component {
+	constructor(props) {
+		super(props);
+		state = {
+			email: '',
+			password: ''
+		};
 	}
-});
 
-class LogInForm extends React.Component {
+	onClickListener = viewId => {
+		Alert.alert('Alert', 'Button pressed ' + viewId);
+	};
+
 	render() {
 		return (
-			<View style={styles.regForm}>
-				<Text style={styles.header}>Log In</Text>
-				<TextInput
-					style={styles.textInput}
-					placeholder="Your Email"
-					underlineColorAndroid={'transparent'}
+			<View style={styles.container}>
+				<SvgUri
+					width="120"
+					height="120"
+					source={require('../icons/logo.svg')}
 				/>
-				<TextInput
-					style={styles.textInput}
-					placeholder="Your Password"
-					underlineColorAndroid={'transparent'}
-				/>
-				<TouchableOpacity style={styles.button}>
-					<Text style={styles.btnText}>Log In</Text>
-				</TouchableOpacity>
+				<View style={styles.inputContainer}>
+					<TextInput
+						style={styles.inputs}
+						placeholder="Email"
+						keyboardType="email-address"
+						onChangeText={email => this.setState({ email })}
+					/>
+				</View>
+
+				<View style={styles.inputContainer}>
+					<TextInput
+						style={styles.inputs}
+						placeholder="Password"
+						secureTextEntry={true}
+						onChangeText={password => this.setState({ password })}
+					/>
+				</View>
+
+				<TouchableHighlight
+					style={[styles.buttonContainer, styles.loginButton]}
+					onPress={() => this.onClickListener('login')}
+				>
+					<Text style={styles.loginText}>Login</Text>
+				</TouchableHighlight>
+
+				<TouchableHighlight
+					style={[styles.buttonContainer, styles.passText]}
+					onPress={() => this.onClickListener('restore_password')}
+				>
+					<Text>Forgot your password?</Text>
+				</TouchableHighlight>
+
+				<TouchableHighlight
+					style={[styles.buttonContainer, styles.regText]}
+					onPress={() => this.onClickListener('register')}
+				>
+					<Text>Register</Text>
+				</TouchableHighlight>
 			</View>
 		);
 	}
 }
 
-export default LogInForm;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	inputContainer: {
+		borderBottomColor: '#000',
+		backgroundColor: '#FFFFFF',
+		borderBottomWidth: 1,
+		width: 250,
+		height: 45,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	inputs: {
+		flex: 1,
+		alignSelf: 'stretch',
+		height: 40,
+		color: '#fff',
+		borderBottomColor: '#f8f8f8',
+		borderBottomWidth: 1
+	},
+	buttonContainer: {
+		height: 45,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 50,
+		marginBottom: 20,
+		width: 250
+	},
+	loginButton: {
+		backgroundColor: '#ff7539'
+	},
+	loginText: {
+		color: 'white'
+	}
+});
