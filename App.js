fde812ba401b8427038a96fd191005436f3dfdb0
osama <enlil96rashid@gmail.com>
2018-07-25 14:25:54 +0200

    can't connect to the backend

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Footer from "./components/Footer";
 import SplashScreen from "./components/SplashScreen";
 
@@ -7,6 +8,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      scans: {},
       loading: true
     };
   }
@@ -22,7 +24,39 @@ class App extends React.Component {
     setTimeout(() => {
       this.hideSplashScreen();
     }, 1000);
+    this.getScans();
   }
+
+  getScans = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append(
+      "Authorization",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9zYW1hQGV4YW1wbGUuY29tIiwiaWQiOiI1YjU3MjRjZDM4NWE4YjBiMzkwMTUyODkiLCJpYXQiOjE1MzI0Mzc5NjEsImV4cCI6MTU2Mzk3Mzk2MX0.eF75GChYGhPDTMP1rNLQObBx1z8O4afMONAH_cHzFOY"
+    );
+    console.log(myHeaders);
+    //10.0.2.2
+    fetch(`172.16.137.115:8080/api/scans`, {
+      method: "get",
+      headers: myHeaders
+    })
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .then(res => {
+        const data = res;
+        console.log(data);
+
+        // this.setState(state => {
+        //   // state.scans = data;
+        //   state.loading = false;
+        //   return state;
+        // });
+      })
+      .catch(err => console.error(err));
+  };
 
   render() {
     return this.state.loading ? <SplashScreen /> : <Footer />;
