c2becc0985b2d6242bc1a175b05abe2ab0bfa7cb
osama <enlil96rashid@gmail.com>
2018-07-25 15:02:58 +0200

    got the scans data from the backend

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      scans: {},
+      data: {},
       loading: true
     };
   }
@@ -29,31 +29,24 @@ class App extends React.Component {
 
   getScans = () => {
     const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Accept", "application/json");
     myHeaders.append(
       "Authorization",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9zYW1hQGV4YW1wbGUuY29tIiwiaWQiOiI1YjU3MjRjZDM4NWE4YjBiMzkwMTUyODkiLCJpYXQiOjE1MzI0Mzc5NjEsImV4cCI6MTU2Mzk3Mzk2MX0.eF75GChYGhPDTMP1rNLQObBx1z8O4afMONAH_cHzFOY"
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9zYW1hQGV4YW1wbGUuY29tIiwiaWQiOiI1YjU3MjRjZDM4NWE4YjBiMzkwMTUyODkiLCJpYXQiOjE1MzI0Mzc5NjEsImV4cCI6MTU2Mzk3Mzk2MX0.eF75GChYGhPDTMP1rNLQObBx1z8O4afMONAH_cHzFOY"
     );
-    console.log(myHeaders);
     //10.0.2.2
-    fetch(`172.16.137.115:8080/api/scans`, {
+    fetch(`http://172.16.137.115:8080/api/scans`, {
       method: "get",
       headers: myHeaders
     })
-      .then(res => {
-        console.log(res);
-        return res.json();
-      })
+      .then(res => res.json())
       .then(res => {
         const data = res;
         console.log(data);
-
-        // this.setState(state => {
-        //   // state.scans = data;
-        //   state.loading = false;
-        //   return state;
-        // });
+        this.setState(state => {
+          state.data = data;
+          state.loading = false;
+          return state;
+        });
       })
       .catch(err => console.error(err));
   };
