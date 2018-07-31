0723fe284cf07864c45d27c16c0399cb5cf401cd
osama <enlil96rashid@gmail.com>
2018-07-31 09:29:13 +0200

    working on the login view

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Footer from "./components/Footer";
+import PrivateNavs from "./components/PrivateNavs";
 import SplashScreen from "./components/SplashScreen";
+import { AsyncStorage } from "react-native";
+import LogInForm from "./components/LogInForm";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,6 +10,7 @@ class App extends React.Component {
 
     this.state = {
       scans: {},
+      token: false,
       loading: true
     };
   }
@@ -20,24 +23,62 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    try {
+      user2 = await AsyncStorage.getItem("token");
+      console.log(user2);
+      return user2;
+    } catch (error) {
+      console.log(error.message);
+    }
+    // this.getScans();
     setTimeout(() => {
       this.hideSplashScreen();
     }, 1000);
-    this.getScans();
   }
+
+  // componentWillMount() {
+  //   // TODO Get AsyncStorage token
+  
+  // }
+
+  loginUser = async user => {
+    // let user = null;
+    console.log(user);
+    
+    //10.0.2.2
+    fetch(`http://172.16.137.115:8080/api/login`, {
+      method: "post",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({
+        email: `${user.email}`,
+        password: `${user.password}`
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        const userData = res;
+        console.log(userData);
+        //TODO AsyncStorage.setItem("token", res.user.token)
+        return userData;
+      })
+      .catch(err => console.error(err));
+  };
 
   getScans = () => {
     //10.0.2.2
-    fetch(`http://"your IP addrs":8080/api/scans`, {
+    fetch(`http://172.16.137.115:8080/api/scans`, {
       method: "get",
       headers: {
-        Authorization: "Bearer 'put a user token here'"
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFhYUBleGFtcGxlLmNvbSIsImlkIjoiNWI1ZWNjMmVlNTQwMDkyMWIzZjg1MjE3IiwiaWF0IjoxNTMyOTM5MzEwLCJleHAiOjE1MzMxMTIxMTB9.PSgQXIJ_Yn0WYy_or7YdYSmqyAL51FtDS2vhaTvJpow"
       }
     })
       .then(res => res.json())
       .then(res => {
         const data = res;
-        console.log(data);
+        // console.log(data);
         this.setState(state => {
           state.scans = data;
           state.loading = false;
@@ -48,7 +89,11 @@ class App extends React.Component {
   };
 
   render() {
-    return this.state.loading ? <SplashScreen /> : <Footer />;
+    return this.state.loading ? (
+      <SplashScreen />
+    ) : (
+      <LogInForm loginUser={this.loginUser} />
+    );
   }
 }
 
--- components/LogInForm.js
+++ components/LogInForm.js
@@ -1,66 +1,88 @@
-import React from 'react';
+import React from "react";
 import {
-	TouchableOpacity,
-	StyleSheet,
-	Text,
-	View,
-	TextInput
-} from 'react-native';
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  AsyncStorage
+} from "react-native";
 
 const styles = StyleSheet.create({
-	regForm: {
-		alignSelf: 'stretch'
-	},
-	header: {
-		fontSize: 20,
-		color: '#fff',
-		paddingBottom: 10,
-		marginBottom: 40,
-		borderBottomColor: '#199187',
-		borderBottomWidth: 1
-	},
-	textInput: {
-		alignSelf: 'stretch',
-		height: 40,
-		marginBottom: 30,
-		color: '#fff',
-		borderBottomColor: '#f8f8f8',
-		borderBottomWidth: 1
-	},
-	button: {
-		alignSelf: 'stretch',
-		alignItems: 'center',
-		padding: 20,
-		backgroundColor: '#0fa1ff',
-		marginTop: 30
-	},
-	btnText: {
-		color: '#fff',
-		fontWeight: 'bold'
-	}
+  regForm: {
+    alignSelf: "stretch",
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 20,
+    color: "#fff",
+    paddingBottom: 10,
+    marginBottom: 40,
+    borderBottomColor: "#199187",
+    borderBottomWidth: 1
+  },
+  textInput: {
+    alignSelf: "stretch",
+    height: 40,
+    marginBottom: 30,
+    color: "black",
+    borderBottomColor: "#f8f8f8",
+    borderBottomWidth: 1
+  },
+  button: {
+    alignSelf: "stretch",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#0fa1ff",
+    marginTop: 30
+  },
+  btnText: {
+    color: "#fff",
+    fontWeight: "bold"
+  }
 });
 
 class LogInForm extends React.Component {
-	render() {
-		return (
-			<View style={styles.regForm}>
-				<Text style={styles.header}>Log In</Text>
-				<TextInput
-					style={styles.textInput}
-					placeholder="Your Email"
-					underlineColorAndroid={'transparent'}
-				/>
-				<TextInput
-					style={styles.textInput}
-					placeholder="Your Password"
-					underlineColorAndroid={'transparent'}
-				/>
-				<TouchableOpacity style={styles.button}>
-					<Text style={styles.btnText}>Log In</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+  submit = async e => {
+    // hard coded for now
+    console.log("hii");
+    const user = {
+      email: "osama@example.com",
+      password: "osama"
+    };
+    // this.setState(state => {
+    //   state.user = user;
+    //   return state;
+    // });
+
+    this.props.loginUser(user);
+  };
+
+  render() {
+    return (
+      <View style={styles.regForm}>
+        <Text style={styles.header}>Log In</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Email"
+          underlineColorAndroid={"transparent"}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Password"
+          underlineColorAndroid={"transparent"}
+        />
+        <Button
+          style={styles.button}
+          onPress={e => this.submit(e)}
+          title="Login"
+          color="#841584"
+          accessibilityLabel="Log in to Organice"
+        />
+      </View>
+    );
+  }
 }
 
 export default LogInForm;
